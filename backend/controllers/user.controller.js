e022d19be92427bf10654c301f038b97d1d307bf
AnushkaChaudhary1322 <anushka1322.be22@chitkara.edu.in>
2025-01-23 00:57:32 +0530

    User Login Controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     try {
@@ -43,8 +44,38 @@ export const login = async (req, res) => {
             });
         }
         let user = await User.findOne({email});
+        if(!user) {
+            return res.status(401).json({
+                message: "Incorrect email or password",
+                success: false,
+            });
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatch) {
+            return res.status(401).json({
+                message: "Incorrect email or password",
+                success: false,
+            });
+        }
 
-        
+        user = {
+            _id:user._id,
+            username: user.username,
+            email:user.email,
+            profilePicture: user.profilePicture,
+            bio: user.bio,
+            followers: user.followers,
+            following: user.following,
+            posts: user.posts
+        }
+
+        const token = await jwt.sign({userId:user._id}, process.env.SECRET_KEY, {expiresIn: 'id'});
+        return res.cookie('token', token, {httpOnly: true, sameSite:'strict', maxAge:1*24*60*60*1000}).json({
+            message:`Welcome back ${user.username}`,
+            success:true,
+            user
+        })
+
     }
     catch (error) {
         console.log(error);
