6e257aaf668d5130ea576cdfd0ca554b6beeef52
AnushkaChaudhary1322 <anushka1322.be22@chitkara.edu.in>
2025-01-24 12:02:28 +0530

    Suggested Users

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -143,4 +143,23 @@ export const editProfile = async (req, res) => {
     catch(error) {
         console.log(error)
     }
-}
+};
+
+export const getSuggestedUsers = async (req, res) => {
+    try {
+        const suggestedUsers = await User.find({_id:{$ne:req.id}}).select("-password");
+        if(!suggestedUsers){
+            return res.status(400).json({
+                message: 'Currently do not have any suggestion',
+            })
+        };
+        return res.status(200).json({
+            success: true,
+            users: suggestedUsers
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+
