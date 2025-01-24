d7cabfcc5bccbf1c4b2386bddfd3e0b7f67d51c0
AnushkaChaudhary1322 <anushka1322.be22@chitkara.edu.in>
2025-01-24 11:54:24 +0530

    Edit profile and cloudinary setup

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import cloudinary from "../utils/cloudinary.js";
 
 export const register = async (req, res) => {
     try {
@@ -110,7 +111,34 @@ export const getProfile = async(req, res) => {
 
 export const editProfile = async (req, res) => {
     try{
-        const 
+        const userId = req.id;
+        const {bio, gender} = req.body;
+        const profilePicture = req.file;
+
+        let cloudResponse;
+        if(profilePicture) {
+            const fileUri = getDataUri(profilePicture);
+            cloudResponse = await cloudinary.uploader.upload(fileUri);
+        }
+
+        const user = await User.findById(userId);
+        if(!user) {
+            return res.status(404).json({
+                message: 'User not found',
+                success: false
+            })
+        };
+        if(bio) user.bio = bio;
+        if(gender) user.gender = gender;
+        if(profile) user.profilePicture = cloudResponse.secure_url;
+
+        await user.save();
+
+        return res.status(200).json({
+            message: 'Profile updated',
+            success: true,
+            user
+        })
     }
     catch(error) {
         console.log(error)
