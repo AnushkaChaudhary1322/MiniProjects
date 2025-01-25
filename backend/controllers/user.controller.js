c54e13d5ccc5cfbf87b58566e4257a2708fd7178
AnushkaChaudhary1322 <anushka1322.be22@chitkara.edu.in>
2025-01-25 23:09:25 +0530

    Follow or Unfollow Controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -163,3 +163,32 @@ export const getSuggestedUsers = async (req, res) => {
     }
 };
 
+// 1:45:21
+
+export const followOrUnfollow = async (req, res) => {
+    try {
+        const followKrneWala = req.id; 
+        const jiskoFollowKrunga = req.params.id; 
+        if (followKrneWala === jiskoFollowKrunga) {
+            return res.status(400).json({
+                message: 'You cannot follow/unfollow yourself',
+                success: false
+            });
+        }
+
+        const user = await User.findById(followKrneWala);
+        const targetUser = await User.findById(jiskoFollowKrunga);
+
+        if (!user || !targetUser) {
+            return res.status(400).json({
+                message: 'User not found',
+                success: false
+            });
+        }
+
+    }
+
+    catch (error) {
+        console.log(error)
+    }
+}
