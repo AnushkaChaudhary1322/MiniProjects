34998db6201cb57fb26c98e0bbf255aed9bf8bc3
AnushkaChaudhary1322 <anushka1322.be22@chitkara.edu.in>
2025-02-03 23:48:15 +0530

    Socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -12,3 +12,25 @@ const io = new Server(server, {
         methods:['GET','POST']
     }
 })
+
+const userSocketMap = {} ; // this map stores socket id corresponding the user id; userId -> socketId
+
+export const getReceiverSocketId = (receiverId) => userSocketMap[receiverId];
+
+io.on('connection', (socket)=>{
+    const userId = socket.handshake.query.userId;
+    if(userId){
+        userSocketMap[userId] = socket.id;
+    }
+
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
+    socket.on('disconnect',()=>{
+        if(userId){
+            delete userSocketMap[userId];
+        }
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    });
+})
+
+export {app, server, io};
